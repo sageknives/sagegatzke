9e1c0b233f3f88323095120c413d99d168c4f026
sageknives <sageknives@gmail.com>
2022-06-20 00:56:28 -0700

    Update gallery.ts

--- src/constants/gallery.ts
+++ src/constants/gallery.ts
@@ -3,11 +3,11 @@ import { Gallery } from "../types";
 export const gallery: Gallery = {
   images: [
     {
-      src: "/images/sageandbishop.jpeg",
+      src: "./images/sageandbishop.jpeg",
       alt: "sage and bishop",
     },
     {
-      src: "/images/sageandwhiteboard.jpeg",
+      src: "./images/sageandwhiteboard.jpeg",
       alt: "sage",
     },
   ],
